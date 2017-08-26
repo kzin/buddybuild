760ca1d206e579287f0e196263fe5df4d3df1f18
Rodrigo Cavalcante <rodrigo.vcavalcante@gmail.com>
2017-08-26 16:55:42 -0300

    add project

---
+++ buddybuild/buddybuild/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  buddybuild
+//
+//  Created by Rodrigo Cavalcante on 26/08/17.
+//  Copyright © 2017 app. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ buddybuild/buddybuildTests/buddybuildTests.swift
@@ -0,0 +1,33 @@
+//
+//  buddybuildTests.swift
+//  buddybuildTests
+//
+//  Created by Rodrigo Cavalcante on 26/08/17.
+//  Copyright © 2017 app. All rights reserved.
+//
+
+import UIKit
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import buddybuild
+
+class buddybuildTests: QuickSpec {
+    
+    override func spec() {
+        
+        var viewController: ViewController!
+        
+        describe("buddybuildTests") {
+            
+            beforeEach {
+                viewController = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "viewController") as! ViewController
+            }
+            
+            it("should have a cool layout") {
+                expect(viewController.view) == snapshot()
+            }
+        }
+    }
+}
